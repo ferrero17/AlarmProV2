a0f70ee7d227ec777e0ac9d4fba3846af4c776e1
dferrero92 <dferrerol92@elpuig.xeill.net>
2018-12-23 22:21:54 +0100

    Estado: RecylerView Implementado, Añadir Alarmas Implementado,  Interfaz de Configuración de Alarma implementada

--- app/src/main/java/com/example/dani/alarmprov2/ConfiguracionAlarmaActivity.java
+++ app/src/main/java/com/example/dani/alarmprov2/ConfiguracionAlarmaActivity.java
@@ -1,7 +1,10 @@
 package com.example.dani.alarmprov2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfiguracionAlarmaActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class ConfiguracionAlarmaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion_alarma);
+
+        Button button = (Button) findViewById(R.id.botonAjustesRepeticion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SwipeActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/dani/alarmprov2/MyAdapter.java
+++ app/src/main/java/com/example/dani/alarmprov2/MyAdapter.java
@@ -1,10 +1,13 @@
 package com.example.dani.alarmprov2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -24,8 +27,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
         Alarma alarma = list.get(i);
 
-      //  myViewHolder.alarmName.setText(alarma.name);
+       //  myViewHolder.alarmName.setText(alarma.name);
        // myViewHolder.alarmTimePick.setText(alarma.timePick);
+
+        //Asignar eventos de los botones:
+        myViewHolder.setOnClickListeners();
+
 
 
     }
@@ -42,17 +49,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView alarmName;
         private TextView alarmTimePick;
+        Button btnConfiguracionAlarma;
+
+        //contexto
+        Context context;
 
 
         public MyViewHolder(@NonNull View itemView) {
 
             super(itemView);
+            context = itemView.getContext();
 
           // alarmName = itemView.findViewById(R.id.alarm_name);
            // alarmTimePick = itemView.findViewById(R.id.alarm_timePick);
+
+            btnConfiguracionAlarma = (Button) itemView.findViewById(R.id.configuracionAlarma);
+
+
+        }
+
+        void setOnClickListeners(){
+
+            btnConfiguracionAlarma.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(context, ConfiguracionAlarmaActivity.class);
+            context.startActivity(intent);
 
         }
     }
